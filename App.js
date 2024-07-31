dc7aac9651567a19d9e8a23b5a8bb1958c4acc95
TomasLin <horsekit1982@gmail.com>
2024-07-31 16:46:22 +0800

    iniital project

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from "react-redux";
+import { NavigationContainer } from "@react-navigation/native";
+import store from "~/store/configureStore";
+import Toast from "react-native-toast-message";
+import AuthScreen from "~/screens/AuthScreen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Provider store={store}>
+        <AuthScreen />
+        <Toast />
+      </Provider>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
